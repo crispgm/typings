eb894f36c657334e42cd7efde6c6e29518c4370e
David Zhang <crispgm@gmail.com>
2020-04-06 00:05:55 +0800

    Publish automatically

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ cp -r src/* docs/
 
 sed -i 's/@import url("\/themes\//@import url("\/typings\/themes\//g' docs/assets/style.css
 sed -i 's/<link href="\/assets\/style.css" rel="stylesheet">/<link href="\/typings\/assets\/style.css" rel="stylesheet">/g' docs/index.html
+
+git add docs
+git commit -m "Published at `date +%s`"
+git push
